7ec5a8dc8d476ee4ff20a45f2aef3bc4770c24a8
Jim Dalrymple <jamesdalrymple@protonmail.com>
2019-09-13 15:59:19 +1000

    showcasing instances defined as non-helper-object with explicit import

--- src/main/scala/ObjectAndClass.scala
+++ src/main/scala/ObjectAndClass.scala
@@ -4,12 +4,16 @@ package classandobject
 class Person(val firstName: String, val lastName: String) {}
 
 object Person {
-  def apply(name: String): Person = ???
+  def apply(name: String): Person = {
+    val fullName = name.split(" ")
+    if (fullName.length == 2) new Person(fullName(0), fullName(1))
+    else new Person("", "")
+  }
 }
 
 // * Pattern Matching
 case class Cat(color: String, food: String)
 
 object ChipShop {
-  def willServe(c: Cat): Boolean = ???
+  def willServe(c: Cat): Boolean = (c.food == "Chips")
 }
--- src/main/scala/Typeclass.scala
+++ src/main/scala/Typeclass.scala
@@ -4,32 +4,60 @@ sealed trait TrafficLight {
   def next: TrafficLight = ???
 }
 
-final case object Red extends TrafficLight
-final case object Green extends TrafficLight
-final case object Yellow extends TrafficLight
+final case object Red extends TrafficLight {
+  override def next: TrafficLight = Green
+}
+final case object Green extends TrafficLight {
+  override def next: TrafficLight = Yellow
+}
+final case object Yellow extends TrafficLight {
+  override def next: TrafficLight = Red
+}
 
 sealed trait LinkedList[A] {
   def apply(index: Int): A = ???
 }
 
-final case class Pair[A](head: A, tail: LinkedList[A]) extends LinkedList[A]
-final case class End[A]() extends LinkedList[A]
+final case class Pair[A](head: A, tail: LinkedList[A]) extends LinkedList[A] {
+  override def apply(index: Int): A =
+    if (index == 0) this.head else this.tail(index - 1)
+}
 
-sealed trait CoLinkedList[A] {
+final case class End[A]() extends LinkedList[A] {
+  override def apply(index: Int): A = throw new Exception("Out of Boundary")
+}
+
+sealed trait CoLinkedList[+A] {
   def apply(index: Int): A = ???
 }
 final case class CoPair[A](head: A, tail: CoLinkedList[A])
     extends CoLinkedList[A]
 final case object CoEnd extends CoLinkedList[Nothing]
 
+// Typeclass signatures go here
 trait JsonWriter[A] {
   def write(in: A): String
 }
 
+// Typeclass instances go here: the type-specific implementations of a given procedure outlined in the trait
+// This will have to be explicitly imported with `import JsonWriterInstances._`
+// This is an alternative to
+object JsonWriterInstances {
+  implicit val personWriter =
+    new JsonWriter[Person] {
+      def write(p: Person): String = {
+        val name = p.name
+        val email = p.email
+        s"""{"name": "$name", "email": "$email"}"""
+      }
+    }
+}
+
+// Typeclass interface goes here: provide a generic function that will match against specific instances in scope
 object JsonWriter {
-  def write[A](a: A): String = ???
+  def write[A](a: A)(implicit w: JsonWriter[A]): String = w.write(a)
   implicit class Ops[A](a: A) {
-    def writeJson: String = ???
+    def writeJson(implicit jsonWriter: JsonWriter[A]): String = ???
   }
 }
 
@@ -38,7 +66,7 @@ final case class Person(name: String, email: String)
 object Person {
   implicit val jsonWriterForPerson: JsonWriter[Person] = ???
   // hint: https://www.scala-lang.org/api/current/scala/math/Ordering$.html#fromLessThan[T](cmp:(T,T)=%3EBoolean):scala.math.Ordering[T]
-  implicit val sortablePerson: Ordering[Person] = ???
+  //implicit val sortablePerson: Ordering[Person] = ???
 }
 
 final case class Cat(name: String, food: String)
